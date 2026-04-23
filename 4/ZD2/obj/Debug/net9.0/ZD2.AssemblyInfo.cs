71d3fa6448530baee1b75bd714b345b125924600
Bestol04 <nikitakorobovpro@gmail.com>
2026-04-23 09:50:31 +0300

    Добавил Урок 5

--- 4/ZD2/obj/Debug/net9.0/ZD2.AssemblyInfo.cs
+++ 4/ZD2/obj/Debug/net9.0/ZD2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZD2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b216a459ee3ef75b84b1b3f0fb775abfed9d6789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6696e4d473e062f29fd803c3798c3fd8f3bc791e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZD2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZD2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
