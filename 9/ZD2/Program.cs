89e1545d77e2c242601c4cf7c71588be31d85529
Bestol04 <nikitakorobovpro@gmail.com>
2026-04-27 09:28:20 +0300

    Добавил урок 9

---
+++ 9/ZD2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+public class TerminalCommand
+{
+    public string CommandText { get; set; }
+    public DateTime Timestamp { get; set; }
+
+    public TerminalCommand(string commandText)
+    {
+        CommandText = commandText;
+        Timestamp = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return $"{Timestamp}: {CommandText}";
+    }
+}
+
+public class TerminalHistory
+{
+    private Stack commandStack = new Stack();
+
+    public void AddCommand(string commandText)
+    {
+        commandStack.Push(new TerminalCommand(commandText));
+    }
+
+    public void UndoLastCommand()
+    {
+        if (commandStack.Count > 0)
+        {
+            TerminalCommand removed = (TerminalCommand)commandStack.Pop();
+            Console.WriteLine($"Удалена команда: {removed.CommandText}");
+        }
+    }
+
+    public void ShowLastCommand()
+    {
+        if (commandStack.Count > 0)
+        {
+            TerminalCommand command = (TerminalCommand)commandStack.Peek();
+            Console.WriteLine($"Последняя команда: {command}");
+        }
+    }
+
+    public void ShowAll()
+    {
+        foreach (TerminalCommand command in commandStack)
+        {
+            Console.WriteLine(command);
+        }
+    }
+
+    public void FindByKeyword(string keyword)
+    {
+        foreach (TerminalCommand command in commandStack)
+        {
+            if (command.CommandText.Contains(keyword))
+                Console.WriteLine($"Найдено: {command}");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        TerminalHistory history = new TerminalHistory();
+
+        history.AddCommand("dir");
+        history.AddCommand("cd Projects");
+        history.AddCommand("git status");
+
+        history.ShowLastCommand();
+        history.FindByKeyword("git");
+
+        history.UndoLastCommand();
+        history.ShowAll();
+    }
+}
---
+++ 9/ZD3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface INotifier<T>
+{
+    void Notify(T message);
+}
+
+public class EmailNotifier<T> : INotifier<T>
+{
+    public void Notify(T message)
+    {
+        Console.WriteLine($"Email отправлен: {message}");
+    }
+}
+
+public class NotifierManager<T>
+{
+    private INotifier<T> notifier;
+
+    public NotifierManager(INotifier<T> notifier)
+    {
+        this.notifier = notifier;
+    }
+
+    public void SendBulk(IEnumerable<T> messages)
+    {
+        foreach (T message in messages)
+        {
+            notifier.Notify(message);
+        }
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        INotifier<string> emailNotifier = new EmailNotifier<string>();
+        NotifierManager<string> manager = new NotifierManager<string>(emailNotifier);
+
+        List<string> messages = new List<string>
+        {
+            "Сообщение 1",
+            "Сообщение 2",
+            "Сообщение 3"
+        };
+
+        manager.SendBulk(messages);
+    }
+}
